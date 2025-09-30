8e5e0795c2af7ac2fd744214ca5a499d9fee368c
YADHRA <yadhra.s.2024.aids@rajalakshmi.edu.in>
2025-09-30 22:12:27 +0530

    Switched from SQLite to PostgreSQL using DATABASE_URL

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 import os
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/complaints.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'yadhra@2006')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://srs_cfbj_user:c6virotY64HWY1wTrCGZ1M2n16r40rKp@dpg-d3e04dnfte5s73f2mmi0-a.singapore-postgres.render.com/srs_cfbj'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
